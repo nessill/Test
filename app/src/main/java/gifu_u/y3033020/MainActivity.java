5b23da3f838fd27583e7d08fc1d34a32d74f35d6
Yuga Ueda <text.demo.script@gmail.com>
2021-05-28 17:21:49 +0900

    Initial commit

---
+++ app/src/main/java/gifu_u/y3033020/MainActivity.java
@@ -0,0 +1,14 @@
+package gifu_u.y3033020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
